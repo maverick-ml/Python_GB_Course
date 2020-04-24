54add5cf382faaad062bdddab6a12c8af2369506
maverick-ml <mratner@yandex.ru>
2020-04-24 20:59:49 +0300

    1

--- lesson_1/zd_3.py
+++
@@ -1,6 +0,0 @@
-# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-num = str(input('Введите число: '))
-summer = int(num) + int(num + num) + int(num + num + num)
-print('Считаем:', num, '+', num + num, '+', num + num + num, '=', summer)
